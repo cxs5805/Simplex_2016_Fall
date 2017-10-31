501d3391870204dad87cd93e76a716ffee7da7c7
MAIN\cxs5805 <cxs5805@g.rit.edu>
2017-10-31 12:20:31 -0400

    Lecture 10/31

--- 18_ManagersAndInstancing/AppClass.cpp
+++ 18_ManagersAndInstancing/AppClass.cpp
@@ -3,27 +3,42 @@ using namespace Simplex;
 void Application::InitVariables(void)
 {
 	////Change this to your name and email
-	//m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+//m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
 
-	////Alberto needed this at this position for software recording.
-	//m_pWindow->setPosition(sf::Vector2i(710, 0));
+////Alberto needed this at this position for software recording.
+//m_pWindow->setPosition(sf::Vector2i(710, 0));
 
-	//Set the position and target of the camera
-	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
-	m_pCameraMngr->SetPositionTargetAndUp(AXIS_Z * 10.0f, ZERO_V3, AXIS_Y);
+//Set the position and target of the camera
+//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
+m_pCameraMngr->SetPositionTargetAndUp(AXIS_Z * 10.0f, ZERO_V3, AXIS_Y);
 
-	//init the camera
-	m_pCamera = new MyCamera();
-	m_pCamera->SetPositionTargetAndUp(
-		vector3(0.0f, 3.0f, 20.0f), //Where my eyes are
-		vector3(0.0f, 3.0f, 19.0f), //where what I'm looking at is
-		AXIS_Y);					//what is up
+//init the camera
+m_pCamera = new MyCamera();
+m_pCamera->SetPositionTargetAndUp(
+	vector3(0.0f, 3.0f, 20.0f), //Where my eyes are
+	vector3(0.0f, 3.0f, 19.0f), //where what I'm looking at is
+	AXIS_Y);					//what is up
 
-	//Get the singleton
-	m_pMyMeshMngr = MyMeshManager::GetInstance();
-	m_pMyMeshMngr->SetCamera(m_pCamera);
-	
-	
+//Get the singleton
+m_pMyMeshMngr = MyMeshManager::GetInstance();
+m_pMyMeshMngr->SetCamera(m_pCamera);
+
+m_pMesh = new MyMesh();
+m_pMesh->GenerateTorus(1.0f, 0.7f, 7, 7, C_BROWN);
+
+
+//for (uint i = 0; i < m_m4List.size(); i++)
+for (uint i = 0; i < 5000; i++)
+{
+	matrix4* pTemp = new matrix4();
+	m_m4List.push_back(pTemp);
+	*m_m4List[i] = glm::translate(IDENTITY_M4, vector3(i * 2, 0, 0)) * ToMatrix4(m_qArcBall);
+}
+
+// can't modify though
+//for (uint i = 0; i < 5000; i++)
+//	m_m4List.push_back(glm::translate(IDENTITY_M4, vector3(i * 2, 0, 0)) * ToMatrix4(m_qArcBall));
+
 }
 void Application::Update(void)
 {
@@ -37,6 +52,7 @@ void Application::Update(void)
 	CameraRotation();
 
 	//Add objects to the Manager
+	/*
 	uint nCount = 0;
 	for (int j = -420; j < 420; j += 2)
 	{
@@ -47,11 +63,38 @@ void Application::Update(void)
 		}
 	}
 	m_pMeshMngr->Print("Objects: " + std::to_string(nCount) + "\n", C_BLUE);
+	//*/
 }
 void Application::Display(void)
 {
 	//Clear the screen
 	ClearScreen();
+
+	// render object we generated
+	// a couple of different ways to do this
+	// could do this
+	//m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
+
+	/*
+	// repeat
+	m_pMesh->Render(m_pCamera, ToMatrix4(m_qArcBall));
+	m_pMesh->Render(m_pCamera, glm::translate(IDENTITY_M4, vector3(1,0,0)) * ToMatrix4(m_qArcBall));
+	m_pMesh->Render(m_pCamera, glm::translate(IDENTITY_M4, vector3(4, 0, 0)) * ToMatrix4(m_qArcBall));
+	for (uint i = 0; i < 5000; i++)
+		m_pMesh->Render(m_pCamera, glm::translate(IDENTITY_M4, vector3(i, 0, 0)) * ToMatrix4(m_qArcBall));
+	//*/
+
+	// better performance, but constructing matrix every frame is bottleneck
+	//for (uint i = 0; i < 5000; i++)
+	//	m4List.push_back(glm::translate(IDENTITY_M4, vector3(i * 2, 0, 0)) * ToMatrix4(m_qArcBall));
+
+	// solution
+	*m_m4List[0] = ToMatrix4(m_qArcBall);
+
+
+	m_pMesh->Render(m_pCamera, m_m4List);
+
+	//m_pMesh->Render();
 
 	//Render the list of MyMeshManager
 	m_pMyMeshMngr->Render();
@@ -64,15 +107,20 @@ void Application::Display(void)
 
 	//clear the render list
 	m_pMeshMngr->ClearRenderList();
-		
+
 	//draw gui
 	DrawGUI();
-	
+
 	//end the current frame (internally swaps the front and back buffers)
 	m_pWindow->display();
 }
 void Application::Release(void)
 {
+	for (uint i = 0; i < m_m4List.size(); i++)
+	{
+		SafeDelete(m_m4List[i]);
+	}
+
 	//release the singleton
 	MyMeshManager::ReleaseInstance();
 
